d41d6588ddea1e17a522efa951c60a7507612b4c
MsPau1a <168515260+MsPau1a@users.noreply.github.com>
2024-04-30 08:30:29 +0200

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
 namespace MagicTurn{
-    export function turnLeft (seconds: number) {
+    export function queenLeft (seconds: number) {
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 40)
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 5)
     basic.pause(seconds)
     maqueen.motorStop(maqueen.Motors.All)
     }
 
-    export function turnRight(seconds2: number) {
+    export function queenRight(seconds2: number) {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 40)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 5)
         basic.pause(seconds2)
