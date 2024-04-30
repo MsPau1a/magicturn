09f3d2932cde9e9b28aa0139edbdc01adcad6752
MsPau1a <168515260+MsPau1a@users.noreply.github.com>
2024-04-30 08:39:28 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,12 @@
-namespace MagicTurn{
-    export function plaLeft (seconds: number) {
-    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 40)
-    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 5)
-    basic.pause(seconds)
+function turnLeft (sec: number) {
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 5)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 40)
+    basic.pause(sec)
     maqueen.motorStop(maqueen.Motors.All)
-    }
-
-    
 }
-
-
-
+function turnRight (sec2: number) {
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 40)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 5)
+    basic.pause(sec2)
+    maqueen.motorStop(maqueen.Motors.All)
+}
