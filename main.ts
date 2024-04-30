9d451d50c16e18a8336a61e82eb4617e8fb41985
MsPau1a <168515260+MsPau1a@users.noreply.github.com>
2024-04-30 08:34:55 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,12 @@
 namespace MagicTurn{
-    export function queenLeft (seconds: number) {
+    export function plaLeft (seconds: number) {
     maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 40)
     maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 5)
     basic.pause(seconds)
     maqueen.motorStop(maqueen.Motors.All)
     }
 
-    export function queenRight(seconds2: number) {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 40)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 5)
-        basic.pause(seconds2)
-        maqueen.motorStop(maqueen.Motors.All)
-    }
-
+    
 }
 
 
